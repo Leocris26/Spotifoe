8d839b1b8410e3868dbb9891ff7f52003460b309
Leonardo Gonzales <leocris26@hotmail.com>
2016-04-27 08:41:08 -0400

    Cambiada la Base De Datos

--- Spotifoe/sql/musica.sql
+++ Spotifoe/sql/musica.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.12
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Apr 20, 2016 at 11:13 PM
--- Server version: 5.6.25
--- PHP Version: 5.6.11
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 27-04-2016 a las 14:38:39
+-- Versión del servidor: 10.1.10-MariaDB
+-- Versión de PHP: 5.6.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `musica`
+-- Base de datos: `musica`
 --
 CREATE DATABASE IF NOT EXISTS `musica` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `musica`;
@@ -25,58 +25,65 @@ USE `musica`;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `album`
+-- Estructura de tabla para la tabla `album`
 --
 
-CREATE TABLE IF NOT EXISTS `album` (
+CREATE TABLE `album` (
   `Cod_album` int(11) NOT NULL,
   `Nro_canciones` int(4) NOT NULL,
   `Nombre` varchar(30) NOT NULL,
   `Ano` int(4) NOT NULL,
   `Duracion` varchar(8) NOT NULL,
   `Fecha_lan` date NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `album`
+-- Volcado de datos para la tabla `album`
 --
 
 INSERT INTO `album` (`Cod_album`, `Nro_canciones`, `Nombre`, `Ano`, `Duracion`, `Fecha_lan`) VALUES
 (1, 16, 'X', 2014, '65:27', '2014-06-20'),
-(2, 14, '25', 2015, '48:25', '2015-11-20');
+(2, 14, '25', 2015, '48:25', '2015-11-20'),
+(3, 13, 'Nevermind', 1992, '46:69', '1991-09-24'),
+(4, 13, 'The open door', 2006, '51:29', '2005-09-25'),
+(5, 10, 'Amar sin Mentiras', 2004, '43:16', '2004-06-08'),
+(7, 17, 'Contraste', 2007, '71:32', '2007-07-24'),
+(8, 11, 'Ciclos', 2009, '46:09', '2009-05-19');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `albumartista`
+-- Estructura de tabla para la tabla `albumartista`
 --
 
-CREATE TABLE IF NOT EXISTS `albumartista` (
+CREATE TABLE `albumartista` (
   `Cod_album` int(11) NOT NULL,
   `Cod_artista` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `albumartista`
+-- Volcado de datos para la tabla `albumartista`
 --
 
 INSERT INTO `albumartista` (`Cod_album`, `Cod_artista`) VALUES
+(1, 2),
 (2, 1),
-(1, 2);
+(3, 7),
+(4, 8);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `artista`
+-- Estructura de tabla para la tabla `artista`
 --
 
-CREATE TABLE IF NOT EXISTS `artista` (
+CREATE TABLE `artista` (
   `Cod_artista` int(11) NOT NULL,
   `Nombre` varchar(30) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `artista`
+-- Volcado de datos para la tabla `artista`
 --
 
 INSERT INTO `artista` (`Cod_artista`, `Nombre`) VALUES
@@ -85,66 +92,74 @@ INSERT INTO `artista` (`Cod_artista`, `Nombre`) VALUES
 (3, 'Shakira'),
 (4, 'Enrique Iglesias'),
 (5, 'Michael Jackson'),
-(6, 'Jason Mraz');
+(6, 'Jason Mraz'),
+(7, 'Nirvana'),
+(8, 'Evanescence');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `artistacancion`
+-- Estructura de tabla para la tabla `artistacancion`
 --
 
-CREATE TABLE IF NOT EXISTS `artistacancion` (
+CREATE TABLE `artistacancion` (
   `Cod_artista` int(11) NOT NULL,
   `Cod_cancion` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `artistacancion`
+-- Volcado de datos para la tabla `artistacancion`
 --
 
 INSERT INTO `artistacancion` (`Cod_artista`, `Cod_cancion`) VALUES
 (1, 1),
-(2, 2);
+(2, 2),
+(7, 3),
+(8, 4);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `cancion`
+-- Estructura de tabla para la tabla `cancion`
 --
 
-CREATE TABLE IF NOT EXISTS `cancion` (
+CREATE TABLE `cancion` (
   `Cod_cancion` int(11) NOT NULL,
-  `CalidadA` varchar(30) NOT NULL,
-  `CalidadB` varchar(30) NOT NULL,
+  `CalidadA` varchar(80) NOT NULL,
+  `CalidadB` varchar(80) NOT NULL,
   `Duracion` varchar(7) NOT NULL,
   `Fecha_lan` date NOT NULL,
   `Veces_escuch` int(15) NOT NULL,
   `Nombre` varchar(30) NOT NULL,
   `Cod_genero` int(11) NOT NULL,
   `Cod_album` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `cancion`
+-- Volcado de datos para la tabla `cancion`
 --
 
 INSERT INTO `cancion` (`Cod_cancion`, `CalidadA`, `CalidadB`, `Duracion`, `Fecha_lan`, `Veces_escuch`, `Nombre`, `Cod_genero`, `Cod_album`) VALUES
 (1, '', '', '4:55', '2015-10-23', 0, 'Hello', 3, 2),
-(2, '', '', '4:40', '2014-08-14', 0, 'Thinking out loud', 4, 1);
+(2, '', '', '4:40', '2014-08-14', 0, 'Thinking out loud', 4, 1),
+(3, '', '', '4:15', '1991-09-24', 0, 'Lithium', 6, 3),
+(4, '', '', '3:48', '2007-02-06', 0, 'Lithium', 7, 4),
+(6, 'C:/xampp/htdocs/canciones/Ahora Quien.mp3', '', '05:11', '2001-04-04', 0, 'Ahora Quien', 8, 5),
+(7, 'C:/xampp/htdocs/canciones/Conteo Regresivo.mp3', '', '4:25', '2003-06-17', 0, 'Conteo Regresivo', 8, 7);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `genero`
+-- Estructura de tabla para la tabla `genero`
 --
 
-CREATE TABLE IF NOT EXISTS `genero` (
+CREATE TABLE `genero` (
   `Cod_genero` int(11) NOT NULL,
   `Nombre` varchar(20) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `genero`
+-- Volcado de datos para la tabla `genero`
 --
 
 INSERT INTO `genero` (`Cod_genero`, `Nombre`) VALUES
@@ -152,87 +167,90 @@ INSERT INTO `genero` (`Cod_genero`, `Nombre`) VALUES
 (2, 'Pop'),
 (3, 'Soul'),
 (4, 'Soft rock'),
-(5, 'Blue-eyed soul');
+(5, 'Blue-eyed soul'),
+(6, 'Grunge'),
+(7, 'Metal Alternativo'),
+(8, 'Salsa');
 
 --
--- Indexes for dumped tables
+-- Índices para tablas volcadas
 --
 
 --
--- Indexes for table `album`
+-- Indices de la tabla `album`
 --
 ALTER TABLE `album`
   ADD PRIMARY KEY (`Cod_album`);
 
 --
--- Indexes for table `albumartista`
+-- Indices de la tabla `albumartista`
 --
 ALTER TABLE `albumartista`
   ADD PRIMARY KEY (`Cod_album`,`Cod_artista`),
   ADD KEY `Cod_artista` (`Cod_artista`);
 
 --
--- Indexes for table `artista`
+-- Indices de la tabla `artista`
 --
 ALTER TABLE `artista`
   ADD PRIMARY KEY (`Cod_artista`);
 
 --
--- Indexes for table `artistacancion`
+-- Indices de la tabla `artistacancion`
 --
 ALTER TABLE `artistacancion`
   ADD PRIMARY KEY (`Cod_artista`,`Cod_cancion`),
   ADD KEY `Cod_cancion` (`Cod_cancion`);
 
 --
--- Indexes for table `cancion`
+-- Indices de la tabla `cancion`
 --
 ALTER TABLE `cancion`
   ADD PRIMARY KEY (`Cod_cancion`);
 
 --
--- Indexes for table `genero`
+-- Indices de la tabla `genero`
 --
 ALTER TABLE `genero`
   ADD PRIMARY KEY (`Cod_genero`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT for table `album`
+-- AUTO_INCREMENT de la tabla `album`
 --
 ALTER TABLE `album`
-  MODIFY `Cod_album` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `Cod_album` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
--- AUTO_INCREMENT for table `artista`
+-- AUTO_INCREMENT de la tabla `artista`
 --
 ALTER TABLE `artista`
-  MODIFY `Cod_artista` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+  MODIFY `Cod_artista` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
--- AUTO_INCREMENT for table `cancion`
+-- AUTO_INCREMENT de la tabla `cancion`
 --
 ALTER TABLE `cancion`
-  MODIFY `Cod_cancion` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `Cod_cancion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
--- AUTO_INCREMENT for table `genero`
+-- AUTO_INCREMENT de la tabla `genero`
 --
 ALTER TABLE `genero`
-  MODIFY `Cod_genero` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+  MODIFY `Cod_genero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
--- Constraints for dumped tables
+-- Restricciones para tablas volcadas
 --
 
 --
--- Constraints for table `albumartista`
+-- Filtros para la tabla `albumartista`
 --
 ALTER TABLE `albumartista`
   ADD CONSTRAINT `albumartista_ibfk_1` FOREIGN KEY (`Cod_album`) REFERENCES `album` (`Cod_album`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `albumartista_ibfk_2` FOREIGN KEY (`Cod_artista`) REFERENCES `artista` (`Cod_artista`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `artistacancion`
+-- Filtros para la tabla `artistacancion`
 --
 ALTER TABLE `artistacancion`
   ADD CONSTRAINT `artistacancion_ibfk_1` FOREIGN KEY (`Cod_cancion`) REFERENCES `cancion` (`Cod_cancion`) ON DELETE CASCADE ON UPDATE CASCADE,
